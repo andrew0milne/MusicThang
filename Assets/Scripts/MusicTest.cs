ae80353bca210da8102003f5774a46ab5c8205d8
andrew0milne <32519506+andrew0milne@users.noreply.github.com>
2018-11-02 16:01:56 +0000

    Readded Everything

---
+++ Assets/Scripts/MusicTest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicTest : MonoBehaviour 
+{
+
+public float BPM;
+public bool[] hits;
+public AudioSource audio;
+
+float beat_interval;
+float time;
+
+	// Use this for initialization
+	void Start () 
+	{
+		beat_interval = 60.0f / BPM;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		time += Time.deltaTime;
+
+if(time >= beat_interval)
+{
+
+time = 0.0f;
+audio.Play();
+}
+	}
+}
